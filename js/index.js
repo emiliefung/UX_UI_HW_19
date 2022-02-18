99829a70332e455b30d0573e437423af1e024854
Emilie Fung <emilie.fung12@gmail.com>
2022-02-17 22:37:51 -0500

    update

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $(".hover1").hover(function() {
+        $(this).css("color", "orange");
+    });
+
+    $(".hover2").hover(function() {
+        $(this).css("color", "orange");
+    });
+
+    $(".hover3").hover(function() {
+        $(this).css("color", "orange");
+    });
+
+    $(".hover4").hover(function() {
+        $(this).css("color", "orange");
+    });
+
+    $(".hover5").hover(function() {
+        $(this).css("color", "orange");
+    });
+
+    $(".hover6").hover(function() {
+        $(this).css("color", "orange");
+    });
+
+    $(".hover7").hover(function() {
+        $(this).css("color", "orange");
+    });
+
+    $(".hover8").hover(function() {
+        $(this).css("color", "orange");
+    });
+
+});
